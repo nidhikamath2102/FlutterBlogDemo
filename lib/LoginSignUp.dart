d18a363e603c12407939cdd9e3ed9a9864630cf4
nidhikamath <kamathnidhi98@gmail.com>
2020-10-05 22:36:48 +0530

    initial commit

--- lib/LoginSignUp.dart
+++ lib/LoginSignUp.dart
@@ -1,17 +1,43 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class LoginSignUp extends StatefulWidget{
-  State<StatefulWidget> createState(){
+class LoginSignUp extends StatefulWidget {
+  State<StatefulWidget> createState() {
     return _LoginSignUpState();
   }
 }
 
-class _LoginSignUpState extends State<LoginSignUp>{
+enum FormType { login, register }
 
-  void login(){
+class _LoginSignUpState extends State<LoginSignUp> {
+  final formKey = new GlobalKey<FormState>();
+  FormType _formType = FormType.login;
+  String _email = "";
+  String _password = "";
+
+  bool validate(){
+    final form = formKey.currentState;
+    if(form.validate()){
+      form.save();
+      return true;
+    }else{
+      return false;
+    }
   }
 
-  void register(){
+  void login() {
+    formKey.currentState.reset();
+    setState(() {
+      _formType = FormType.login;
+    });
+  }
+
+  void register() {
+    formKey.currentState.reset();
+    setState(() {
+      _formType = FormType.register;
+    });
   }
 
   @override
@@ -23,60 +49,110 @@ class _LoginSignUpState extends State<LoginSignUp>{
       body: new Container(
         margin: EdgeInsets.all(20.0),
         child: new Form(
+          key: formKey,
           child: new Column(
             crossAxisAlignment: CrossAxisAlignment.center,
-            children: inputWidgets(),
+            children: inputWidgets() + buttons(),
           ),
         ),
       ),
     );
   }
 
-  List<Widget> inputWidgets(){
-    return[
-      SizedBox(height: 10.0,),
+  List<Widget> inputWidgets() {
+    return [
+      SizedBox(
+        height: 10.0,
+      ),
       logo(),
-      SizedBox(height: 30.0,),
-
+      SizedBox(
+        height: 30.0,
+      ),
       new TextFormField(
-        decoration: new InputDecoration(
-          labelText: "Email"
-        ),
+        decoration: new InputDecoration(labelText: "Email"),
+        validator: (val) {
+          return val.isEmpty ? 'Email is mandatory' : null;
+        },
+        onSaved: (email) {
+          return _email = email;
+        },
       ),
-      SizedBox(height: 10.0,),
+      SizedBox(
+        height: 10.0,
+      ),
       new TextFormField(
-        decoration: new InputDecoration(
-            labelText: "Password"
-        ),
+        decoration: new InputDecoration(labelText: "Password"),
+        validator: (value) {
+          return value.isEmpty ? 'Password is needed' : null;
+        },
+        onSaved: (password) {
+          return _password = password;
+        },
+        obscureText: true,
       ),
-      SizedBox(height: 40.0,),
-      new RaisedButton(
-        child: new Text(
-          "Login",
-          style: new TextStyle(
-            fontSize: 20.0
-          ),
-        ),
-        textColor: Colors.white,
-        color: Colors.red,
-        onPressed: login,
-      ),
-      SizedBox(height: 10.0,),
-      new FlatButton(
-        child: new Text(
-          "Sign Up",
-          style: new TextStyle(
-              fontSize: 14.0
-          ),
-        ),
-        textColor: Colors.red,
-        color: Colors.white,
-        onPressed: register,
+      SizedBox(
+        height: 40.0,
       ),
     ];
   }
 
-  Widget logo(){
+  List<Widget> buttons() {
+    if(_formType == FormType.login){
+      return[
+        new RaisedButton(
+          child: new Text(
+            "Login",
+            style: new TextStyle(
+                fontSize: 20.0
+            ),
+          ),
+          textColor: Colors.white,
+          color: Colors.red,
+          onPressed: validate,
+        ),
+        SizedBox(height: 10.0,),
+        new FlatButton(
+          child: new Text(
+            "Do not have an account? Sign Up",
+            style: new TextStyle(
+                fontSize: 14.0
+            ),
+          ),
+          textColor: Colors.red,
+          color: Colors.white,
+          onPressed: register,
+        ),
+      ];
+    }else{
+      return[
+        new RaisedButton(
+          child: new Text(
+            "Create An Account",
+            style: new TextStyle(
+                fontSize: 20.0
+            ),
+          ),
+          textColor: Colors.white,
+          color: Colors.red,
+          onPressed: validate,
+        ),
+        SizedBox(height: 10.0,),
+        new FlatButton(
+          child: new Text(
+            "Have an account? Log In",
+            style: new TextStyle(
+                fontSize: 14.0
+            ),
+          ),
+          textColor: Colors.red,
+          color: Colors.white,
+          onPressed: login,
+        ),
+      ];
+    }
+  }
+
+  Widget logo() {
     return new Hero(
       tag: 'Hero',
       child: new CircleAvatar(
@@ -86,5 +162,4 @@ class _LoginSignUpState extends State<LoginSignUp>{
       ),
     );
   }
-
 }
